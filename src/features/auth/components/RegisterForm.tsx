3935110e81b9cdb278f1bead71513d7480d7c7b5
Ramon Rasheed <ramonrash2@gmail.com>
2024-06-13 21:08:32 +0100

    "Refactor RegisterForm component to use react-hook-form and zod schema validation"

--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -6,11 +6,31 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { registerSchema } from "@/lib/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { FaGithub, FaGoogle } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { z } from "zod";
+
+export type RegisterFormValues = z.input<typeof registerSchema>;
 export default function RegisterForm() {
+  const form = useForm<RegisterFormValues>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  function onSubmit(values: RegisterFormValues) {
+    console.log(values);
+  }
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
@@ -20,51 +40,108 @@ export default function RegisterForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid grid-cols-2 gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="first-name">First name</Label>
-              <Input id="first-name" placeholder="Max" required />
+        <div className="space-y-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <div className="flex items-center gap-x-4">
+                <FormField
+                  name="first_name"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Firstname</FormLabel>
+                      <FormControl>
+                        <Input id="first-name" placeholder="Max" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  name="last_name"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Lastname</FormLabel>
+                      <FormControl>
+                        <Input
+                          id="last-name"
+                          placeholder="Robinson"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormField
+                name="email"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        id="email"
+                        placeholder="me@example.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="password"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input id="password" type="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="re_password"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input id="re_password" type="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                Create an account
+              </Button>
+            </form>
+          </Form>
+          <div className="space-y-2 grid">
+            <span className="text-center text-sm uppercase">
+              OR Continue with
+            </span>
+            <div className="flex items-center gap-x-3">
+              <Button variant="outline" className="w-full">
+                <FaGoogle />
+              </Button>
+              <Button variant="outline" className="w-full">
+                <FaGithub />
+              </Button>
             </div>
-            <div className="grid gap-2">
-              <Label htmlFor="last-name">Last name</Label>
-              <Input id="last-name" placeholder="Robinson" required />
-            </div>
           </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <Link to="/login" className="underline">
+              Sign in
+            </Link>
           </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-          <Button type="submit" className="w-full">
-            Create an account
-          </Button>
-
-          <span className="text-center text-sm uppercase">
-            OR Continue with
-          </span>
-          <div className="flex items-center gap-x-3">
-            <Button variant="outline" className="w-full">
-              <FaGoogle />
-            </Button>
-            <Button variant="outline" className="w-full">
-              <FaGithub />
-            </Button>
-          </div>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Already have an account?{" "}
-          <Link to="/login" className="underline">
-            Sign in
-          </Link>
         </div>
       </CardContent>
     </Card>
