65bf0764015870eb0567acbba69d48d2f23313bf
Ramon Rasheed <ramonrash2@gmail.com>
2024-06-14 10:43:29 +0100

    "Replace RouterProvider with BrowserRouter and add App component"

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import Home from "./pages/Home";
+import AccountVerification from "./pages/auth/AccountVerification";
+import { LoginPage } from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ResendActivation from "./pages/auth/ResendActivation";
+import { ResetPassword } from "./pages/auth/ResetPassword";
+
+export default function App() {
+  return (
+    <Routes>
+      {/* Protected Routes */}
+      <Route path="/" element={<ProtectedRoutes />}>
+        <Route index element={<Home />} />
+      </Route>
+      {/* Public Routes */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/resend-activation" element={<ResendActivation />} />
+      <Route
+        path="/auth/activate/:uid/:token"
+        element={<AccountVerification />}
+      />
+    </Routes>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import { AuthProvider } from "./components/AuthProvider";
 import { Toaster } from "./components/ui/sonner";
 import "./index.css";
-import router from "./routes";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </AuthProvider>
       <Toaster
         richColors
